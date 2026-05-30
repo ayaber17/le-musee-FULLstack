df57b617ead3916e61a685514cf4c61290f65337
ayaber17 <berraouiaya17@gmail.com>
2026-05-30 02:50:55 +0100

    update corse

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://le-musee-ful-lstack-xo2x.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
