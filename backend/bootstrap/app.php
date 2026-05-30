4b1e6d9387f87e734f19229da40b3718d8121a2e
ayaber17 <berraouiaya17@gmail.com>
2026-05-30 03:33:22 +0100

    fix: add CORS middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -21,6 +21,30 @@
 
 
 
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
+
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         api: __DIR__.'/../routes/api.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+// ->withMiddleware(function (Middleware $middleware) {
+//     $middleware->statefulApi();
+//     $middleware->alias([
+//         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+//         'role'  => \App\Http\Middleware\RoleMiddleware::class,
+
+//     ]);
+// })
+//     ->withExceptions(function (Exceptions $exceptions) {
+//     })
+//     ->create();
+
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -32,14 +56,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-->withMiddleware(function (Middleware $middleware) {
-    $middleware->statefulApi();
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'role'  => \App\Http\Middleware\RoleMiddleware::class,
-
-    ]);
-})
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'role'  => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
     })
     ->create();
