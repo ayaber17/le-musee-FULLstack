b37b7d8a4e2090bfd899f7e081abfc4e4f9be042
ayaber17 <berraouiaya17@gmail.com>
2026-05-30 20:25:31 +0100

    fix: CORS in router.php

--- backend/public/router.php
+++ backend/public/router.php
@@ -1,17 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://le-musee-ful-lstack-xo2x.vercel.app');
-header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit();
+if (php_sapi_name() === 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
 }
 
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
-    return false;
-}
 require_once __DIR__ . '/index.php';
